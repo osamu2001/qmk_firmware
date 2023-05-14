db671a18448dad3643229930e0a40748b6881753
Osamu Okano <okano.osamu@mercari.com>
2023-05-14 14:35:35 +0900

    remove

--- keyboards/keyball/keyball44/keymaps/takashicompany/keymap.c
+++ keyboards/keyball/keyball44/keymaps/takashicompany/keymap.c
@@ -27,8 +27,8 @@ enum custom_keycodes {
     KC_MY_BTN1 = KEYBALL_SAFE_RANGE,
     KC_MY_BTN2,
     KC_MY_BTN3,
-    KC_MY_BTN4,
-    KC_MY_BTN5,
+//    KC_MY_BTN4,
+//    KC_MY_BTN5,
     KC_MY_SCR,
     KC_TO_CLICKABLE_INC,
     KC_TO_CLICKABLE_DEC
